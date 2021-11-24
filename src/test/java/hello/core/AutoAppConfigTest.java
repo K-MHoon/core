89fe821785dbe825117c4903eec2bb900e31bd1d
K-MHOON <azmria117@gmail.com>
2021-11-24 09:39:18 +0900

    feat : 컴포넌트 스캔
    - 컴포넌트 스캔을 통해 스프링 컨테이너에 빈으로 등록하는 방법을 테스트
    - 중복 빈 발생시 충돌나는 경우를 테스트
    - @Component 로 컴포넌트 스캔의 대상들로 설정
    - excludeFilters/includeFilters = @Filter 통해서 컴포넌트 스캔 대상이 되거나 예외처리하는 방법 추가

---
+++ src/test/java/hello/core/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertInstanceOf(MemberService.class, memberService);
+    }
+}
---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,34 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * 컴포넌트 스캔을 통해서 Bean이 등록되는지 안되는지 테스트
+ */
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertNotNull(beanA);
+
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("beanB", BeanB.class));
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+    }
+}
